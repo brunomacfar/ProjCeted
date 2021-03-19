02094bde13c2c60aacedb31eb992d7475af2a8ef
brunomacfar <brunomacfar@gmail.com>
2021-03-19 09:29:36 -0300

    Atualização back-end

--- ProjAPI/Startup.cs
+++ ProjAPI/Startup.cs
@@ -30,7 +30,8 @@ namespace ProjAPI
         public void ConfigureServices(IServiceCollection services)
         {
            services.AddDbContext<ProjCetedContext>(
-               c => c.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), providerOptions => providerOptions.EnableRetryOnFailure() ));
+           //    c => c.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), providerOptions => providerOptions.EnableRetryOnFailure() ));
+               c => c.UseMysql(Configuration.GetConnectionString("DefaultConnection"), providerOptions => providerOptions.EnableRetryOnFailure() ));
            services.AddScoped<IProjRepositorio, RepositorioProj>();  
            services.AddControllers().AddNewtonsoftJson(); 
            services.AddCors();
